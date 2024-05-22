e02a6d6ba1bfb0399508af3d14cdca96ecc01081
daniel behbudi <mbehbudi@gmail.com>
2024-05-22 09:00:39 +0200

    d

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Table } from "react-bootstrap";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const token = localStorage.getItem("token");
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/user", {
+          headers: {
+            Authorization: `bearer ${token}`,
+          },
+        });
+        const result = await response.json();
+        setUsers(result);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (token) {
+      fetchUsers();
+    } else {
+      navigate("/login");
+    }
+  }, [token, navigate]);
+
+  return (
+    <>
+      <Container>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </>
+  );
 }
 
 export default Dashboard;
